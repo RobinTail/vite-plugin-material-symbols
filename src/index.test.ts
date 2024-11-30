ffa2eef0490d06cedb369060eb52b3eb517944d9
Robin Tail <robin_tail@me.com>
2024-11-30 14:40:59 +0100

    Fix: wrong biome.

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,4 @@
-import {describe, it, expect} from "bun:test";
+import { describe, expect, it } from "bun:test";
 import plugin from "./index";
 
 describe("Entrypoint", () => {
@@ -9,4 +9,4 @@ describe("Entrypoint", () => {
   it("should return object of certain structure", () => {
     expect(plugin()).toMatchSnapshot();
   });
-})
+});
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,21 @@
-import type {Plugin} from "vite";
-import esquery, {type Literal} from "esquery";
+import esquery, { type Literal } from "esquery";
+import type { Plugin } from "vite";
 
 const plugin = (): Plugin => {
-  const registry = new Set<string>;
+  const registry = new Set<string>();
 
-  return ({
+  return {
     name: "material-symbols",
     enforce: "pre",
-    moduleParsed: function ({id, ast}) {
+    moduleParsed: function ({ id, ast }) {
       if (!ast) return;
       const nodes = esquery.query(
         ast,
         "CallExpression[callee.name='jsx'][arguments.0.name='Icon'] > .arguments > Property[key.name='children'] Literal",
       ) as unknown as Literal[];
-      this.debug({id, message: "value"});
-      for (const {value} of nodes) if (typeof value === "string") registry.add(value);
+      this.debug({ id, message: "value" });
+      for (const { value } of nodes)
+        if (typeof value === "string") registry.add(value);
     },
     transformIndexHtml: (html) =>
       html.replace(
@@ -23,7 +24,7 @@ const plugin = (): Plugin => {
           ? `icon_names=${Array.from(registry.values()).toSorted().join(",")}`
           : "", // dev mode, all icons
       ),
-  });
+  };
 };
 
 export default plugin;
