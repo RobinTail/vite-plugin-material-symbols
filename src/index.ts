1e2bab472d489a16ac4e7233c072b8421b9e8188
Robin Tail <robin_tail@me.com>
2024-11-30 14:29:29 +0100

    Making it a function and further configurable.

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,29 @@
 import type {Plugin} from "vite";
 import esquery, {type Literal} from "esquery";
 
-const registry = new Set<string>;
+const plugin = (): Plugin => {
+  const registry = new Set<string>;
 
-const plugin: Plugin = {
-  name: "material-symbols",
-  enforce: "pre",
-  moduleParsed: function ({ id, ast }) {
-    if (!ast) return;
-    const nodes = esquery.query(
-      ast,
-      "CallExpression[callee.name='jsx'][arguments.0.name='Icon'] > .arguments > Property[key.name='children'] Literal",
-    ) as unknown as Literal[];
-    this.debug({ id, message: "value" });
-    for (const { value } of nodes) if (typeof value === "string") registry.add(value);
-  },
-  transformIndexHtml: (html) =>
-    html.replace(
-      "__MATERIAL_SYMBOLS__",
-      registry.size
-        ? `icon_names=${Array.from(registry.values()).toSorted().join(",")}`
-        : "", // dev mode, all icons
-    ),
+  return ({
+    name: "material-symbols",
+    enforce: "pre",
+    moduleParsed: function ({id, ast}) {
+      if (!ast) return;
+      const nodes = esquery.query(
+        ast,
+        "CallExpression[callee.name='jsx'][arguments.0.name='Icon'] > .arguments > Property[key.name='children'] Literal",
+      ) as unknown as Literal[];
+      this.debug({id, message: "value"});
+      for (const {value} of nodes) if (typeof value === "string") registry.add(value);
+    },
+    transformIndexHtml: (html) =>
+      html.replace(
+        "__MATERIAL_SYMBOLS__",
+        registry.size
+          ? `icon_names=${Array.from(registry.values()).toSorted().join(",")}`
+          : "", // dev mode, all icons
+      ),
+  });
 };
 
 export default plugin;
