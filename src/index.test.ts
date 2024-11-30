e80462854abe38755d43fcbc0cdf21f075b577b5
Anna Bocharova <robin_tail@me.com>
2024-11-30 15:10:13 +0100

    Initial setup (#1)
    * Initial setup using bun.
    
    * Add biome linter.
    
    * Making it a function and further configurable.
    
    * Basic CI.
    
    * Moving vite to peers.
    
    * Coverage.
    
    * Fix: wrong biome.
    
    * Build.
    
    * Building with tsdown, checking with attw.
    
    * Fix package by npm.

---
+++ src/index.test.ts
@@ -0,0 +1,12 @@
+import { describe, expect, it } from "bun:test";
+import plugin from "./index";
+
+describe("Entrypoint", () => {
+  it("should be a function", () => {
+    expect(plugin).toBeFunction();
+  });
+
+  it("should return object of certain structure", () => {
+    expect(plugin()).toMatchSnapshot();
+  });
+});
