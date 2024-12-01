9eeca3e49ea120da0abd35e94af470ddcf028367
Robin Tail <robin_tail@me.com>
2024-12-01 11:27:44 +0100

    Moving tools into a dedicated folder.

--- tools/vite.config.ts
+++ tools/vite.config.ts
@@ -1,8 +1,13 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-import materialSymbols from "./src";
+import materialSymbols from "../src";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: "./tools/index.html",
+    },
+  },
   plugins: [react(), materialSymbols()],
 });
