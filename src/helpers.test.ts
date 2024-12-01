65a8dc6a392358f198707c9504ad0350b0553504
Robin Tail <robin_tail@me.com>
2024-12-01 13:09:21 +0100

    Test for defaultUrlProvider.

--- src/helpers.test.ts
+++ src/helpers.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from "bun:test";
 import type { Node } from "estree";
-import { isStringLiteral } from "./helpers.ts";
+import { defaultUrlProvider, isStringLiteral } from "./helpers.ts";
 
 describe("Helpers", () => {
   describe("isStringLiteral", () => {
@@ -12,6 +12,14 @@ describe("Helpers", () => {
       { type: "Identifier", name: "test" },
     ])("invalidates others %#", (subject) => {
       expect(isStringLiteral(subject)).toBeFalse();
+    });
+  });
+
+  describe("defaultUrlProvider", () => {
+    it("returns the default Material Symbols CDN URL with supplied param injected", () => {
+      expect(defaultUrlProvider("test")).toBe(
+        "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&test",
+      );
     });
   });
 });
