23374ba32244ea368b2e0719297fd49298039ac3
Robin Tail <robin_tail@me.com>
2024-12-01 13:10:33 +0100

    Test for makeSelector.

--- src/helpers.test.ts
+++ src/helpers.test.ts
@@ -1,6 +1,10 @@
 import { describe, expect, it } from "bun:test";
 import type { Node } from "estree";
-import { defaultUrlProvider, isStringLiteral } from "./helpers.ts";
+import {
+  defaultUrlProvider,
+  isStringLiteral,
+  makeSelector,
+} from "./helpers.ts";
 
 describe("Helpers", () => {
   describe("isStringLiteral", () => {
@@ -19,6 +23,14 @@ describe("Helpers", () => {
     it("returns the default Material Symbols CDN URL with supplied param injected", () => {
       expect(defaultUrlProvider("test")).toBe(
         "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&test",
+      );
+    });
+  });
+
+  describe("makeSelector", () => {
+    it("return a selector having component name injected", () => {
+      expect(makeSelector("Test")).toBe(
+        "CallExpression[callee.name='jsx'][arguments.0.name='Test'] > .arguments > Property[key.name='children'] Literal",
       );
     });
   });
