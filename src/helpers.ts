b8bc2509d5c06de8a25a35680bb1646a6a1b7372
Robin Tail <robin_tail@me.com>
2026-03-16 08:28:39 +0100

    ref: ext calleeName.

--- src/helpers.ts
+++ src/helpers.ts
@@ -10,12 +10,14 @@ export const isStringLiteral = (
 export const defaultUrlProvider = (iconNamesParam: string) =>
   `https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&${iconNamesParam}`;
 
-export const makeSelector = (jsxNodeRegex: RegExp, component: string) =>
-  [
-    `CallExpression[callee.name=/${jsxNodeRegex.source}/${jsxNodeRegex.flags}][arguments.0.name='${component}']`,
+export const makeSelector = (jsxNodeRegex: RegExp, component: string) => {
+  const calleeName = `/${jsxNodeRegex.source}/${jsxNodeRegex.flags}`;
+  return [
+    `CallExpression[callee.name=${calleeName}][arguments.0.name='${component}']`,
     ".arguments",
     "Property[key.name='children'] Literal",
   ].join(" > ");
+};
 
 /**
  * @see https://www.measurethat.net/Benchmarks/Show/12088/3/new-array-from-vs-slice-vs-push-vs-index-vs-spread
